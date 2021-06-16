422a31d0c9fe8a90e5b6c758b4f6f3715c84312f
EkaterinaKavit <84904881+EkaterinaKavit@users.noreply.github.com>
2021-06-16 15:05:58 +0300

    Add files via upload

---
+++ trying_dataframe.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df=pd.read_excel('first_try_excel.xlsx', engine='openpyxl')
+
+#df1=df.iloc[3:5,2:5]
+#df1.to_excel('statistics1.xlsx')
+#df['PARAMETER']
+#df[['Lot','WAFER']]
