53ebdd6edb49e5907423b075a351f12278c43b73
EkaterinaKavit <84904881+EkaterinaKavit@users.noreply.github.com>
2021-10-12 16:32:35 +0300

    Add files via upload
    Не работает цикл, сейчас выдает ошибку в drop_duplicates по поводу добавления ignore_index

--- pickle_DNEPMMUL.py
+++ pickle_DNEPMMUL.py
@@ -5,25 +5,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Read pickle
 df=pd.read_pickle('F8_32_2.pickle')
-df_for_analise=df[df['PARAMETER']=='DNEPMMUL']
 
-#print(type(df_for_analise))
+#choose unique parameters from the column "PARAMETER"
+df_without_duplicates=df['PARAMETER'].drop_duplicates(ignore_index=True)
+
+#Create xlsx file with unique
+df_without_duplicates.to_excel('unique_parameters.xlsx')
+a=len(df_without_duplicates)
+#print(a)
+
+
+for i in df_without_duplicates:
+    df_for_analise=df_without_duplicates[i]
+    print(df_for_analise)
+    #df_for_analise=df[df['PARAMETER']=='BNN1010BTA']
+
 
 #making the dataframe with the meanings for all lots and sites for one PARAMETER
-df_site = df_for_analise['6 site']
-
-# Отладка
-#print(df_site.iloc[1:10])
-#a=df_site.iloc[1:20]
-#b=df_for_analise['15 site'].iloc[1:20]
-#c=a.append(b, ignore_index=True)
-#print(c)
-
-df_site1 = df_site.append(df_for_analise['15 site'],ignore_index=True)
-df_site2 = df_site1.append(df_for_analise['17 site'],ignore_index=True)
-df_site3 = df_site2.append(df_for_analise['19 site'],ignore_index=True)
-df_site4 = df_site3.append(df_for_analise['28 site'],ignore_index=True)
+#df_site = df_for_analise['6 site']
+#df_site1 = df_site.append(df_for_analise['15 site'],ignore_index=True)
+#df_site2 = df_site1.append(df_for_analise['17 site'],ignore_index=True)
+#df_site3 = df_site2.append(df_for_analise['19 site'],ignore_index=True)
+#df_site4 = df_site3.append(df_for_analise['28 site'],ignore_index=True)
 #print(df_site4)
 
 # check n/A meanings
@@ -31,23 +36,23 @@ df_site4 = df_site3.append(df_for_analise['28 site'],ignore_index=True)
 
 
 #add all information for parameter BNN1014RON
-df_for_analise1=df[df['PARAMETER']=='BNN1014RON']
-df_site_1 = df_for_analise1['6 site']
+#df_for_analise1=df[df['PARAMETER']=='QBDFIABHCPSA']
+#df_site_1 = df_for_analise1['6 site']
 # Отладка
 #a1=df_site_1.iloc[1:20]
 #b1=df_for_analise1['15 site'].iloc[1:20]
 #c1=a.append(b, ignore_index=True)
 #print(c1)
 
-df_site1_ = df_site_1.append(df_for_analise1['15 site'],ignore_index=True)
-df_site2_ = df_site1_.append(df_for_analise1['17 site'],ignore_index=True)
-df_site3_ = df_site2_.append(df_for_analise1['19 site'],ignore_index=True)
-df_site4_ = df_site3_.append(df_for_analise1['28 site'],ignore_index=True)
+#df_site1_ = df_site_1.append(df_for_analise1['15 site'],ignore_index=True)
+#df_site2_ = df_site1_.append(df_for_analise1['17 site'],ignore_index=True)
+#df_site3_ = df_site2_.append(df_for_analise1['19 site'],ignore_index=True)
+#df_site4_ = df_site3_.append(df_for_analise1['28 site'],ignore_index=True)
 #print (df_site4_)
 
-result = pd.concat([df_site4,df_site4_],ignore_index=True,axis=1)
+#result = pd.concat([df_site4,df_site4_],ignore_index=True,axis=1)
 #print (type(result))
-result=result.set_axis(['DNEPMMUL','BNN1014RON'],axis=1,inplace=False)
+#result=result.set_axis(['BNN1010BTA','QBDFIABHCPSA'],axis=1,inplace=False)
 #print(result)
 
 # Отладка
@@ -55,18 +60,19 @@ result=result.set_axis(['DNEPMMUL','BNN1014RON'],axis=1,inplace=False)
 #print(result)
 
 #graph
-fig = plt.figure(figsize = (7,4))
-ax_3d=fig.add_subplot(projection='3d')
+#fig = plt.figure(figsize = (7,4))
+#ax_3d=fig.add_subplot(projection='3d')
 
-x=np.linspace(0,10000,100)
-y=np.linspace(0,10000,100)
+#y=np.linspace(1,10310,10310)
+#x=np.linspace(1,2,2)
 
-xgrid,ygrid=np.meshgrid(x,y)
-zgrid=np.array(result)
 
-ax_3d.plot_wireframe(xgrid,ygrid,zgrid)
-ax_3d.set_xlabel('x')
-ax_3d.set_ylabel('y')
-ax_3d.set_zlabel('z')
-plt.show()
+#xgrid,ygrid=np.meshgrid(x,y)
+#zgrid=np.array(result)
+
+#ax_3d.plot_wireframe(xgrid,ygrid,zgrid)
+#ax_3d.set_xlabel('x')
+#ax_3d.set_ylabel('y')
+#ax_3d.set_zlabel('z')
+#plt.show()
 
